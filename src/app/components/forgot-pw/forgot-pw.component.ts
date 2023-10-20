31e26ff48992a55611b0021551f2eec5ec93f138
sammkinng <fm568pk@gmail.com>
2023-10-20 11:44:31 +0530

    cleansing

--- src/app/components/forgot-pw/forgot-pw.component.ts
+++ src/app/components/forgot-pw/forgot-pw.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-forgot-pw',
-  templateUrl: './forgot-pw.component.html',
-  styleUrls: ['./forgot-pw.component.scss']
+  templateUrl: './forgot-pw.component.html'
 })
 export class ForgotPwComponent {
 
