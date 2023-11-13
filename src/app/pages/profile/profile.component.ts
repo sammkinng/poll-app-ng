378e4bd6ef651829d8bbc306d423f983f643ec52
sammkinng <fm568pk@gmail.com>
2023-11-13 10:41:35 +0530

    formbuilder

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { CountryService } from 'src/app/services/country.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,8 +9,31 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+
+  clickedG=false
+  clickedC=false
+  countries:{
+    code: string;
+    name: string;
+  }[]=[]
+
+  form:FormGroup=this.fb.group({
+    fName:[null,[Validators.required]],
+    lName:[null,[Validators.required]],
+    country:["",[Validators.required]],
+    state:[null,[Validators.required]],
+    gender:["",[Validators.required]],
+    district:[null,[Validators.required]],
+    religion:[null,[Validators.required]],
+    dob:[null,[Validators.required]],
+  })
+
  constructor(
-  public auth:AuthService
- ){}
+  public auth:AuthService,
+  private fb:FormBuilder,
+  private cs:CountryService
+ ){
+  this.countries=cs.getCountries()
+ }
 
 }
